0137bb0153097ce14d8dfcc4ac2c57f9a216861f
Tommy Long <pete_201@hotmail.com>
2015-07-15 21:26:36 +0100

    Introduced Test Project Introduced standardised solution structure

--- tests/SimpleContainer.Tests/Properties/AssemblyInfo.cs
+++ tests/SimpleContainer.Tests/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Tests")]
+[assembly: AssemblyTitle("SimpleContainer.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Tests")]
+[assembly: AssemblyProduct("SimpleContainer.Tests")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("28c0870f-c391-4afb-b60e-a04087fc15a9")]
+[assembly: Guid("8b44c4b5-8373-463e-bec5-6533325a9ff3")]
 
 // Version information for an assembly consists of the following four values:
 //
